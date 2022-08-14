88eeabacd67828f56c2814fbf576dfe2adfbf34c
abhishek18124 <abhishek18124@iiitd.ac.in>
2022-08-14 11:16:46 +0530

    Lecture 39

---
+++ August/Lecture 39 - 14thAugust2022/003Tries_Common.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<unordered_map>
+
+using namespace std;
+
+int main() {
+
+	string words[] = {"flow", "floor", "fly"};
+
+	trie t;
+	for(string word : words) {
+		t.insert(word);
+	}
+
+    // todo ...
+
+	return 0;
+}
---
+++ August/Lecture 39 - 14thAugust2022/006Tries_MaximumSubarrayXOR.cpp
@@ -0,0 +1,70 @@
+/*
+
+Given an array of non-negative integers, find the maximum subarray XOR.
+
+Example: 
+	Input : x[] = {25, 10, 2, 8, 5, 3} 
+	Output: 
+
+*/
+
+#include<iostream>
+#include<climits>
+
+using namespace std;
+
+class node {
+public:
+	
+	node* left;  // indicates if node has a child that represents bit 0
+	node* right; // indicates if node has a child that represents bit 1
+	
+	node() {
+		this->left = NULL;
+		this->right= NULL;
+	}
+};
+
+class trie {
+	
+	node* root;
+
+public:
+	
+	trie() {
+		root = new node();
+	}
+
+	void insert(int n) {
+		node* temp = root;
+		for(int i=31; i>=0; i--) {
+			int bit = (1<<i)&n;
+			if(!bit) {
+				// ith bit of n is 0, check if temp node has a left child or not
+				if(!temp->left) {
+					node* n = new node();
+					temp->left = n;
+				}
+				temp = temp->left;
+			} else {
+				// ith bit of n is 1, check if temp node has a right child or not
+				if(!temp->right) {
+					node* n = new node();
+					temp->right = n;
+				}
+				temp = temp->right;
+			}
+		}
+	}
+
+};
+
+int main() {
+
+	int arr[] = {25, 10, 2, 8, 5, 3};
+	int n = sizeof(arr) / sizeof(int);
+
+	// todo ...
+  
+	return 0;
+}
